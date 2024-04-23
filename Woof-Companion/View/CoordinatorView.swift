ec714614e3056daa9b8c3f4459bebffc15a04463
xNumero3 <notnumero3@gmail.com>
2024-04-23 18:10:50 +0200

    add coordinator view & tab view

--- Woof-Companion/View/CoordinatorView.swift
+++ Woof-Companion/View/CoordinatorView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct CoordinatorView: View {
+    
+    @StateObject private var coordinator = Coordinator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $coordinator.path) {
+            TabView(selection: $coordinator.selectedTab) {
+                CoordinatorTabView(tab: .history)
+                    .tabItem {
+                        Label("History", systemImage: "clock")
+                    }
+                    .tag(AppView.history)
+                
+                CoordinatorTabView(tab: .main)
+                    .tabItem {
+                        Label("Main", systemImage: "house")
+                    }
+                    .tag(AppView.main)
+                
+                CoordinatorTabView(tab: .walk)
+                    .tabItem {
+                        Label("Walk", systemImage: "figure.walk.motion")
+                    }
+                    .tag(AppView.walk)
+            }
+            .navigationDestination(for: AppView.self) { appView in
+                coordinator.build(appView: appView)
+            }
+            .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreenCover in
+                coordinator.build(fullScreenCover: fullScreenCover)
+            }
+        }
+        .environmentObject(coordinator)
+    }
+}
+
+struct CoordinatorTabView: View {
+    @EnvironmentObject var coordinator: Coordinator
+    let tab: AppView
+    
+    var body: some View {
+        switch tab {
+        case .main:
+            coordinator.build(appView: .main)
+        case .walk:
+            coordinator.build(appView: .walk)
+        case .history:
+            coordinator.build(appView: .history)
+        }
     }
 }
 

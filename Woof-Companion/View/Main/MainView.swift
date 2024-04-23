2e95ddb1ef85da4f7aae95434c9136f2a1237af2
xNumero3 <notnumero3@gmail.com>
2024-04-23 17:09:53 +0200

    add main view

--- Woof-Companion/View/Main/MainView.swift
+++ Woof-Companion/View/Main/MainView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            TotalActivityView()
+            LastWalkView()
+        }
+        .background(.red)
+        
     }
 }
 

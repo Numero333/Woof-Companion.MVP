138ed182478675885284156afae674c63c84a7f6
xNumero3 <notnumero3@gmail.com>
2024-04-23 16:33:01 +0200

    Initial Commit

---
+++ Woof-Companion/Woof_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Woof_CompanionApp.swift
+//  Woof-Companion
+//
+//  Created by François-Xavier on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Woof_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Woof-CompanionUITests/Woof_CompanionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Woof_CompanionUITestsLaunchTests.swift
+//  Woof-CompanionUITests
+//
+//  Created by François-Xavier on 23/04/2024.
+//
+
+import XCTest
+
+final class Woof_CompanionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

60cc48098c79adeb47012d881c526514d4eec06a
xNumero3 <notnumero3@gmail.com>
2024-04-23 18:09:28 +0200

    add walk view

--- Woof-Companion/View/WalkView/WalkView.swift
+++ Woof-Companion/View/WalkView/WalkView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct WalkView: View {
+    
+    @EnvironmentObject private var coordinator: Coordinator
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Walking on a dream !")
+            
+            Button {
+                coordinator.present(fullScreenCover: .finishedWalk)
+                print("toggle fullscreen")
+            } label: {
+                Text("FullScren Test Mode")
+            }
+        }
     }
 }
 

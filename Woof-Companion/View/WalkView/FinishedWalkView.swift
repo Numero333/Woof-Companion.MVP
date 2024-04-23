eecea2fd1a025d6ff39fcb26773f329123434db3
xNumero3 <notnumero3@gmail.com>
2024-04-23 18:11:49 +0200

    add finished walk

--- Woof-Companion/View/WalkView/FinishedWalkView.swift
+++ Woof-Companion/View/WalkView/FinishedWalkView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct FinishedWalkView: View {
+    
+    @EnvironmentObject private var coordinator: Coordinator
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("La ballade était agréable ?")
+        
+        Button {
+            coordinator.dismissFullScreenCover()
+        } label: {
+            Text("FullScren Close")
+        }
     }
 }
 

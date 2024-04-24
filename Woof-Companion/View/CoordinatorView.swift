d54dd8d95bd709baa8242a56de9f0b2f365cf047
xNumero3 <notnumero3@gmail.com>
2024-04-24 10:57:48 +0200

    add comments

--- Woof-Companion/View/CoordinatorView.swift
+++ Woof-Companion/View/CoordinatorView.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 
+// A view that manages navigation and tabbed interface using a coordinator pattern.
 struct CoordinatorView: View {
     
-    @StateObject private var coordinator = Coordinator()
+    // StateObject to manage the state and logic of navigation throughout the app.
+    @StateObject private var coordinator = CoordinatorManager()
     
     var body: some View {
+        // Navigation stack to manage navigable views controlled by the coordinator.
         NavigationStack(path: $coordinator.path) {
+            // Tab view for handling user navigation between different app sections.
             TabView(selection: $coordinator.selectedTab) {
                 CoordinatorTabView(tab: .history)
                     .tabItem {
@@ -29,25 +33,29 @@ struct CoordinatorView: View {
                 CoordinatorTabView(tab: .walk)
                     .tabItem {
                         Label("Walk", systemImage: "figure.walk.motion")
+                            .tag(AppView.walk)
                     }
-                    .tag(AppView.walk)
+                // Navigation destination for managing transitions.
+                    .navigationDestination(for: AppView.self) { appView in
+                        coordinator.build(appView: appView) // Builds the appropriate view.
+                    }
+                // Full screen cover for presenting modal views.
+                    .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreenCover in
+                        coordinator.build(fullScreenCover: fullScreenCover)
+                    }
             }
-            .navigationDestination(for: AppView.self) { appView in
-                coordinator.build(appView: appView)
-            }
-            .fullScreenCover(item: $coordinator.fullScreenCover) { fullScreenCover in
-                coordinator.build(fullScreenCover: fullScreenCover)
-            }
+            // Injects the coordinator as an environment object available to child views.
+            .environmentObject(coordinator)
         }
-        .environmentObject(coordinator)
     }
 }
 
+// Represents a tab view managed by the coordinator with specific content.
 struct CoordinatorTabView: View {
-    @EnvironmentObject var coordinator: Coordinator
-    let tab: AppView
-    
+    @EnvironmentObject var coordinator: CoordinatorManager
+    let tab: AppView // Specifies the type of content to build.
     var body: some View {
+        // Constructs the view based on the specified tab.
         switch tab {
         case .main:
             coordinator.build(appView: .main)
@@ -62,3 +70,4 @@ struct CoordinatorTabView: View {
 #Preview {
     CoordinatorView()
 }
+

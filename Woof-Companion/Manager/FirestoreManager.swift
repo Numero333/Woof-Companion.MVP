a05902b7fbe0eb931acaff2a8af0a5acbe0bc36d
xNumero3 <notnumero3@gmail.com>
2024-04-25 14:57:23 +0200

    add comments

--- Woof-Companion/Manager/FirestoreManager.swift
+++ Woof-Companion/Manager/FirestoreManager.swift
@@ -10,15 +10,16 @@ import FirebaseFirestore
 
 final class FirestoreManager {
     
+    // Init Data base
     let db = Firestore.firestore()
 
-    // Création ou mise à jour d'un document
+    // Create a document
     func setDocument(collectionPath: String, documentId: String, data: [String: Any]) async throws {
         let documentRef = db.collection(collectionPath).document(documentId)
         try await documentRef.setData(data)
     }
 
-    // Récupération d'un document spécifique
+    // Fetch a specific document
     func getDocument(collectionPath: String, documentId: String) async throws -> DocumentSnapshot {
         let documentRef = db.collection(collectionPath).document(documentId)
         let documentSnapshot = try await documentRef.getDocument()
@@ -29,13 +30,13 @@ final class FirestoreManager {
         }
     }
 
-    // Suppression d'un document
+    // Delete a document
     func deleteDocument(collectionPath: String, documentId: String) async throws {
         let documentRef = db.collection(collectionPath).document(documentId)
         try await documentRef.delete()
     }
 
-    // Récupération de tous les documents dans une collection
+    // Fetch all document
     func getAllDocuments(collectionPath: String) async throws -> [QueryDocumentSnapshot] {
         let collectionRef = db.collection(collectionPath)
         let querySnapshot = try await collectionRef.getDocuments()
--- Woof-Companion/Manager/WalkDataStoreManager.swift
+++ Woof-Companion/Manager/WalkDataStoreManager.swift
@@ -12,7 +12,7 @@ final class WalkDataStoreManager {
     
     private let firestoreManager = FirestoreManager()
     
-    // Créer un nouveau document pour l'utilisateur
+    // Create a new document for the user
     func createNewDocumentForUser(user: AuthDataResultModel) async throws {
         var userData: [String : Any] = ["uid": user.uid]
         if let userEmail = user.email {
@@ -21,7 +21,7 @@ final class WalkDataStoreManager {
         try await firestoreManager.setDocument(collectionPath: "users", documentId: user.uid, data: userData)
     }
     
-    // Ajouter une nouvelle promenade pour un utilisateur
+    // Add a new walk for the user
     func addNewWalkForUser(user: AuthDataResultModel, walkData: [String: String]) async throws {
         guard walkData["date"] != nil, walkData["distance"] != nil, walkData["duration"] != nil, walkData["location"] != nil, walkData["weather"] != nil else {
             throw NSError(domain: "InvalidWalkData", code: 400, userInfo: ["message": "Missing walk data attributes"])
@@ -29,17 +29,17 @@ final class WalkDataStoreManager {
         try await firestoreManager.setDocument(collectionPath: "users/\(user.uid)/walk", documentId: UUID().uuidString, data: walkData)
     }
     
-    // Récupérer toutes les promenades d'un utilisateur
+    // Fetch all walks of the user
     func fetchAllWalksForUser(uid: String) async throws -> [QueryDocumentSnapshot] {
         return try await firestoreManager.getAllDocuments(collectionPath: "users/\(uid)/walk")
     }
     
-    // Récupérer une promenade spécifique par son ID
+    // Get a specific walk by id
     func fetchWalkById(uid: String, walkId: String) async throws -> DocumentSnapshot {
         return try await firestoreManager.getDocument(collectionPath: "users/\(uid)/walk", documentId: walkId)
     }
     
-    // Supprimer une promenade spécifique
+    // Delete a specific walk by id
     func deleteWalkById(uid: String, walkId: String) async throws {
         try await firestoreManager.deleteDocument(collectionPath: "users/\(uid)/walk", documentId: walkId)
     }

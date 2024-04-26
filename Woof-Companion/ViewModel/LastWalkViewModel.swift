4dbf572a914c8c505fff87b69c4861afcafd12a8
xNumero3 <notnumero3@gmail.com>
2024-04-26 03:04:07 +0200

    add subview viewmodel

--- Woof-Companion/ViewModel/LastWalkViewModel.swift
+++ Woof-Companion/ViewModel/LastWalkViewModel.swift
@@ -5,14 +5,96 @@
 //  Created by François-Xavier on 26/04/2024.
 //
 
-import SwiftUI
+import Foundation
 
-struct LastWalkViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class LastWalkViewModel: ObservableObject {
+    
+    //MARK: - Properties
+    @Published var lastWalk: WalkModel?
+    @Published var errorMessage: String?
+    private var walkStored: [WalkModel] = []
+    private var walkDataStoreManager: WalkDataStoreManager
+    private var authManager: AuthManager
+    private var user: AuthDataResultModel?
+    
+    //MARK: - Initialization
+    init(walkDataStoreManager: WalkDataStoreManager = WalkDataStoreManager(), authManager: AuthManager = AuthManager()){
+        self.walkDataStoreManager = walkDataStoreManager
+        self.authManager = authManager
     }
-}
-
-#Preview {
-    LastWalkViewModel()
+    
+    var energy: String {
+        guard let energy = self.lastWalk?.energy else { return "N/A"}
+        return String(describing: energy) + " %"
+    }
+    
+    var weather: String {
+        guard let weather = self.lastWalk?.weather else { return "N/A" }
+        return String(describing: weather)
+    }
+    
+    var encounter: String {
+        guard let encounter = self.lastWalk?.encounter else { return "N/A" }
+        return String(describing: encounter)
+    }
+    
+    var startDate: String {
+        guard let startDate = self.lastWalk?.startDate else { return "N/A" }
+        return String(describing: startDate)
+    }
+    
+    var duration: String {
+        return formatTime()
+    }
+    
+    var distance: String {
+        guard let distance = self.lastWalk?.distance else { return "N/A" }
+        
+        return formatDistance(distance: distance) + " km"
+    }
+    
+    //MARK: - Methods
+    func fetch() {
+        walkStored = []
+        Task {
+            do {
+                self.user = try authManager.getAuthUser()
+                guard let user = user else {
+                    self.errorMessage = NSError(domain: "History", code: -1).localizedDescription
+                    return
+                }
+                let walksReceived = try await walkDataStoreManager.fetchAllWalksForUser(uid: user.uid)
+                for walk in walksReceived {
+                    let walkReceived = try await walkDataStoreManager.fetchWalkById(uid: user.uid, walkId: walk.documentID)
+                    if let data = walkReceived.data() {
+                        let walkDataDecoded = WalkModel.from(dictionary: data)
+                        walkStored.append(walkDataDecoded)
+                    }
+                }
+                walkStored = walkStored.reversed()
+                DispatchQueue.main.async {
+                    self.lastWalk = self.walkStored.first
+                }
+            } catch let error {
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+    
+    //MARK: - Private
+    private func formatDistance(distance: Double) -> String {
+        let kilometers = distance / 1000.0
+        let formattedDistance = String(format: "%.2f", kilometers)
+        return formattedDistance
+    }
+    
+    private func formatTime() -> String {
+        guard let seconds = self.lastWalk?.duration else { return "N/A" }
+        let convertedSecond = Int(seconds)
+        let hours = convertedSecond / 3600
+        let minutes = (convertedSecond % 3600) / 60
+        let hourString = hours > 0 ? "\(hours)h" : ""
+        let minuteString = "\(minutes)m"
+        return hourString + minuteString
+    }
 }
--- Woof-Companion/ViewModel/TotalActivityViewModel.swift
+++ Woof-Companion/ViewModel/TotalActivityViewModel.swift
@@ -5,14 +5,106 @@
 //  Created by François-Xavier on 26/04/2024.
 //
 
-import SwiftUI
+import Foundation
 
-struct TotalActivityViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+final class TotalActivityViewModel: ObservableObject {
+        
+    //MARK: - Properties
+    @Published var recentWalks: [WalkModel] = []
+    @Published var errorMessage: String?
+    private var walkDataStoreManager: WalkDataStoreManager
+    private var authManager: AuthManager
+    private var user: AuthDataResultModel?
+    
+    //MARK: - Initialization
+    init(walkDataStoreManager: WalkDataStoreManager = WalkDataStoreManager(), authManager: AuthManager = AuthManager()){
+        self.walkDataStoreManager = walkDataStoreManager
+        self.authManager = authManager
     }
-}
+    
+    var energy: String {
+        return calculateEnergy()
+    }
+    
+    var distance: String {
+        return calculateDistance()
+    }
+    
+    var duration: String {
+        return calculateDuration()
+    }
+    
+    var encouter: String {
+        return calculateEncounter()
+    }
+    
+    //MARK: - Methods
+    func fetch() {
+        
+        let calendar = Calendar.current
+        let twentyFourHoursAgo = calendar.date(byAdding: .hour, value: -24, to: Date())!
+        
+        Task {
+            do {
+                self.user = try authManager.getAuthUser()
+                guard let user = user else {
+                    self.errorMessage = NSError(domain: "History", code: -1).localizedDescription
+                    return
+                }
+                let walksReceived = try await walkDataStoreManager.fetchAllWalksForUser(uid: user.uid)
+                for walk in walksReceived {
+                    let walkReceived = try await walkDataStoreManager.fetchWalkById(uid: user.uid, walkId: walk.documentID)
+                    if let data = walkReceived.data() {
+                        let walkDataDecoded = WalkModel.from(dictionary: data)
+                        if walkDataDecoded.date >= twentyFourHoursAgo {
+                            DispatchQueue.main.async {
+                                self.recentWalks.append(walkDataDecoded)
+                            }
+                        }
+                    }
+                }
+            } catch let error {
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+    
+    //MARK: - Private
+    private func calculateEnergy() -> String {
+        var totalEnergy = recentWalks.reduce(0) { $0 + Int($1.energy) }
+        if recentWalks.count > 0 {
+            totalEnergy = totalEnergy / recentWalks.count
+        }
+        return String(totalEnergy) + " %"
+    }
+    
+    private func calculateDistance() -> String {
+        let totalDistance = round(recentWalks.reduce(0) { $0 + $1.distance })
+        return formatDistance(distance: totalDistance) + " km"
+    }
 
-#Preview {
-    TotalActivityViewModel()
+    private func calculateDuration() -> String {
+        let totalDuration = round(recentWalks.reduce(0) { $0 + $1.duration })
+        return formatTime(seconds: totalDuration)
+    }
+
+    private func calculateEncounter() -> String {
+        let totalEncounter = recentWalks.reduce(0) { $0 + $1.encounter }
+        return String(totalEncounter)
+    }
+    
+    private func formatDistance(distance: Double) -> String {
+        let kilometers = distance / 1000.0
+        let formattedDistance = String(format: "%.2f", kilometers)
+        return formattedDistance
+    }
+    
+    private func formatTime(seconds: Double) -> String {
+        let convertedSecond = Int(seconds)
+        let hours = convertedSecond / 3600
+        let minutes = (convertedSecond % 3600) / 60
+        let hourString = hours > 0 ? "\(hours)h " : ""
+        let minuteString = "\(minutes)m"
+        return hourString + minuteString
+    }
 }
